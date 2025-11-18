3a8270b923beeb250eedcbcc9fd59fde59161868
angelesTecnica7 <angeles@tecnica7.edu.ar>
2025-11-18 09:43:33 -0300

    preparado para deploy

--- config/conexion.js
+++ config/conexion.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 //pool de conexiones en vez de createConection porque el pool permite que api no se sature con muchas consultas simultaneas
   const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'latienda',
+    host: process.env.MYSQL_ADDON_HOST || 'localhost',
+    user: process.env.MYSQL_ADDON_USER || 'root',
+    password: process.env.MYSQL_ADDON_PASSWORD || '',
+    database: process.env.MYSQL_ADDON_DB || 'latienda',
     connectionLimit: 5
   });
 
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from "cookie-parser";   
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
